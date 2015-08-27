07d06bcd7cf32361d57f4cfe383ab2e44056ef18
Benedikt von St. Vieth <b.von.st.vieth@fz-juelich.de>
2015-08-27 10:01:15 +0200

    move to common module.

--- updater.py
+++ updater.py
@@ -4,21 +4,27 @@ import logging
 import signal
 
 from datetime import datetime
-from metahosting.common import argument_parsing, config_manager, logging_setup
-from queue_manager import subscribe
 from time import sleep, ctime
+
+from metahosting.common import argument_parsing, config_manager, logging_setup
 
 
 class Updater(object):
     def __init__(self, config):
-        persistence_class = config_manager.get_backend_class(config)
-        self.datapoints = persistence_class(config=config)
+        persistence_class = \
+            config_manager.get_backend_class(config['persistence'])
+        messaging_class = \
+            config_manager.get_backend_class(config['messaging'])
+
+        self.messaging = messaging_class(config=config['messaging'],
+                                         queue='status')
+        self.datapoints = persistence_class(config=config['persistence'])
         self.running = False
 
     def start(self):
         self.running = True
         logging.info('Starting status updater %s', ctime())
-        subscribe('status', self.store_datapoint)
+        self.messaging.subscribe('status', self.store_datapoint)
         while self.running:
             logging.info('Heartbeat status updater %s', ctime())
             sleep(15)
@@ -29,13 +35,13 @@ class Updater(object):
 
     def store_datapoint(self, item):
         timestamp = item.pop('ts')
-        item['date'] = datetime.fromtimestamp(timestamp)
+        item['ts'] = datetime.fromtimestamp(timestamp)
         logging.debug(item)
         self.datapoints.insert(item)
 
     def cleanup_old_entries(self, days=14):
         old = datetime.datetime.now() - datetime.timedelta(days=days)
-        self.datapoints.delete_many({"date": {"$lt": old}})
+        self.datapoints.delete_many({"ts": {"$lt": old}})
 
 
 def run():
@@ -47,7 +53,10 @@ def run():
     if arguments.envfile:
         config_manager._VARIABLES_FILE = arguments.envfile
 
-    config = config_manager.get_configuration('persistence')
+    config = dict()
+    config['persistence'] = config_manager.get_configuration('persistence')
+    config['messaging'] = config_manager.get_configuration('messaging')
+
     updater = Updater(config=config)
 
     signal.signal(signal.SIGTERM, updater.stop)
